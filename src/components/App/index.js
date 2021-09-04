d4d048cf771de1d4deb35603c74d18664a2e4152
felipe-brando <f.brandotalero@gmail.com>
2021-09-04 19:37:48 +0200

    composant racine

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,11 @@
 // == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
+    <div>Formulaire</div>
+    <div>Compteur</div>
+    <div>Liste des héros</div>
   </div>
 );
 
