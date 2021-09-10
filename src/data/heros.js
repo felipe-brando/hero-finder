d3707abe1b4aad7f707870b60cd791539bb67067
felipe-brando <f.brandotalero@gmail.com>
2021-09-10 11:41:29 +0200

    fake data pour tests

---
+++ src/data/heros.js
@@ -0,0 +1,121 @@
+/* eslint-disable */
+export default {
+
+  "groups": [ 
+    { 
+      id: 1,
+      name: "Avengers",
+    },
+    {
+      id: 2,
+      name: "X-men",
+    }
+  ],
+
+  "countries": [
+    {
+      id: 1,
+      name: "Afrique du Sud",
+    },
+    {
+      id: 1,
+      name: "Australie",
+    }, 
+    {
+      id: 1,
+      name: "Norvège",
+    }, 
+    {
+      id: 1,
+      name: "Royaume-Uni",
+    },
+    {
+      id: 4,
+      name: "Russie",
+    },
+    {
+      id: 3,
+      name: "Transia",
+    },
+    {
+      id: 2,
+      name: "USA",
+    }, 
+    {
+      id: 5,
+      name:"Wakanda",
+    }, 
+    
+    
+  ],
+  "heros": [
+    {
+      name: 'THOR',
+      group: 'Avengers',
+      country: 'Norvège',
+      avatar: 'https://terrigen-cdn-dev.marvel.com/content/prod/1x/004tho_com_crd_01.jpg'
+    },
+    {
+      name: 'BLACK PANTHER',
+      group: 'Avengers',
+      country: 'Wakanda',
+      avatar: 'https://terrigen-cdn-dev.marvel.com/content/prod/1x/007blp_com_crd_01_0.jpg'
+    },
+    {
+      name: 'CAPTAIN AMERICA',
+      group: 'Avengers',
+      country: 'USA',
+      avatar: 'https://terrigen-cdn-dev.marvel.com/content/prod/1x/003cap_com_crd_01.jpg'
+    },
+    {
+      name: 'SCARLET WITCH',
+      group: 'Avengers',
+      country: 'Transia',
+      avatar: 'https://terrigen-cdn-dev.marvel.com/content/prod/1x/012scw_com_crd_01.jpg'
+    },
+    {
+      name: 'SHE-HULK',
+      group: 'Avengers',
+      country: 'USA',
+      avatar: 'https://terrigen-cdn-dev.marvel.com/content/prod/1x/123shh_com_crd_01.jpg'
+    },
+    {
+      name: 'BLACK WIDOW',
+      group: 'Avengers',
+      country: 'Russie',
+      avatar: 'https://terrigen-cdn-dev.marvel.com/content/prod/1x/123shh_com_crd_01.jpg'
+    },
+    {
+      name: 'PROFESSOR X',
+      group: 'X-men',
+      country: 'USA',
+      avatar: 'https://terrigen-cdn-dev.marvel.com/content/prod/1x/xavier.jpg'
+    },
+    {
+      name: 'MAGIK',
+      group: 'X-men',
+      country: 'Russie',
+      avatar: 'https://terrigen-cdn-dev.marvel.com/content/prod/1x/303mgk_com_crd_01.jpg'
+    },
+    {
+      name: 'LIFEGUARD',
+      group: 'X-men',
+      country: 'Australie',
+      avatar: 'https://terrigen-cdn-dev.marvel.com/content/prod/1x/lifeguard11.jpg'
+    },
+    {
+      name: 'CHAMBER',
+      group: 'X-men',
+      country: 'Royaume-Uni',
+      avatar: 'https://terrigen-cdn-dev.marvel.com/content/prod/1x/chamber_head.jpg'
+    },
+    {
+      name: 'MAGGOTT',
+      group: 'X-men',
+      country: 'Afrique du Sud',
+      avatar: 'https://terrigen-cdn-dev.marvel.com/content/prod/1x/maggott.jpg'
+    },
+    
+    
+]
+};
